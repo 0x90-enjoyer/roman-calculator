97a5ff6013e9a50e599853d663f08ead3104e9e8
danielduong8 <59457434+danielduong8@users.noreply.github.com>
2021-08-13 15:07:55 -0400

    Removed commented code

--- app/src/main/java/com/example/daniel/RomanCalculator/ExpressionDisplay.java
+++ app/src/main/java/com/example/daniel/RomanCalculator/ExpressionDisplay.java
@@ -17,22 +17,6 @@ public class ExpressionDisplay extends Display {
         }
         else {
             view.setText(exp.getExpression());
-//            StringBuilder sb = new StringBuilder();
-//            for (int i = 0; i < exp.getValues().size(); i++) {
-//                Numeral value = exp.getValues().get(i);
-//                char operator = exp.getOperators().get(i);
-//                if (exp.getValueTypes().get(i).equals("int")) {
-//                    sb.append(String.valueOf(value.getValue()));
-//                }
-//                else if (exp.getValueTypes().get(i).equals("String")) {
-//                    sb.append(value.getRoman());
-//                }
-//                sb.append(" ");
-//                sb.append(operator);
-//                sb.append(" ");
-//            }
-//            text = sb.toString();
-//            view.setText(text);
         }
     }
 }
